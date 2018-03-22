89fa2609a77992a293ad1fc1d0714f43fa01cf48
Ali Emir Sen <senaliemir@gmail.com>
2018-03-22 21:22:28 +0300

    finished

---
+++ src/components/todo/TodoList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { TodoItem } from './TodoItem';
+import PropTypes from 'prop-types';
+
+export const TodoList = props => {
+  return (
+    <div className="Todo-List">
+      <ul>
+        {props.todos.map(todo => (
+          <TodoItem
+            handleRemove={props.handleRemove}
+            handleToggle={props.handleToggle}
+            key={todo.id}
+            {...todo}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+TodoList.propTypes = {
+  todos: PropTypes.array.isRequired
+};
